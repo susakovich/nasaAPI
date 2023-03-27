f0b844c96c6d4406062bc4708510bac1b8f5ea64
Danijel <susakovich@gmail.com>
2023-03-27 23:03:32 +0200

    Refactor old code to async/await

--- js/main.js
+++ js/main.js
@@ -2,27 +2,50 @@
 document.querySelector("button").addEventListener("click", getPhoto);
 document.querySelector("img").src = "";
 
-function getPhoto() {
+async function getPhoto() {
   const choice = document.querySelector("input").value;
-  console.log(choice); // testing to see is it working
-
   const url = `https://api.nasa.gov/planetary/apod?api_key=bvIv6NgdIkAB15Ma4rUrWWtaQczJ1qnFdz3NzqIU&date=${choice}`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // to check containing properties in object
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-      // add conditional if media type isn't image, since there are videos also
-      if (data.media_type === "image") {
-        document.querySelector("img").src = data.hdurl;
-      } else if (data.media_type === "video") {
-        document.querySelector("iframe").src = data.url;
-      }
-
-      document.querySelector("h2").innerText = data.title;
+    if (data.media_type === "image") {
       document.querySelector("img").src = data.hdurl;
-      document.querySelector("p").innerText = data.explanation;
-    })
-    .catch((err) => console.log(`error ${err}`));
+    } else if (data.media_type === "video") {
+      document.querySelector("iframe").src = data.url;
+    }
+
+    document.querySelector("h2").innerText = data.title;
+    document.querySelector("img").src = data.hdurl;
+    document.querySelector("p").innerText = data.explanation;
+  } catch (err) {
+    console.log(`Error is ${err}`);
+  }
 }
+
+// function getPhoto() {
+//   const choice = document.querySelector("input").value;
+//   console.log(choice); // testing to see is it working
+
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=bvIv6NgdIkAB15Ma4rUrWWtaQczJ1qnFdz3NzqIU&date=${choice}`;
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data); // to check containing properties in object
+
+//       // add conditional if media type isn't image, since there are videos also
+//       if (data.media_type === "image") {
+//         document.querySelector("img").src = data.hdurl;
+//       } else if (data.media_type === "video") {
+//         document.querySelector("iframe").src = data.url;
+//       }
+
+//       document.querySelector("h2").innerText = data.title;
+//       document.querySelector("img").src = data.hdurl;
+//       document.querySelector("p").innerText = data.explanation;
+//     })
+//     .catch((err) => console.log(`error ${err}`));
+// }
